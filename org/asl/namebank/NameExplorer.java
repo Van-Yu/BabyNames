8be8a675439269c6c43958a1dce0889c4d33047b
Kent Collins <kent.r.collins@gmail.com>
2018-01-27 17:53:06 +0000

    added the easy tasks

--- org/asl/namebank/NameExplorer.java
+++ org/asl/namebank/NameExplorer.java
@@ -3,8 +3,17 @@ package org.asl.namebank;
 public class NameExplorer {
 
 	/**
-	 * Task 1.
-	 * Find the most popular name for a year according to gender.
+	 * Task 1. Find the most popular name for a year, regardless of gender.
+	 * 
+	 * @param year
+	 * @return the name with the most occurrences
+	 */
+	public String mostPopularNameForYear(String year) {
+		return null;
+	}
+
+	/**
+	 * Task 2. Find the most popular name for a year, filtered by gender.
 	 * 
 	 * @param year
 	 * @param gender
@@ -14,7 +23,38 @@ public class NameExplorer {
 			String gender) {
 		return null;
 	}
-	
-	
+
+	/**
+	 * Task 3. Return an array holding the top ten names for the year, in decreasing
+	 * popularity, regardless of gender.
+	 * 
+	 * @param year
+	 * @return an array holding, in order, the ten most popular names for that year
+	 */
+	public String[] topTenForYear(String year) {
+		return null;
+	}
+
+	/**
+	 * Task 4. Return an approximate count of the total number of babies, by gender,
+	 * for the specified year.
+	 * 
+	 * @param year
+	 * @return the number of babies born with the indicated gender
+	 */
+	public int numBabiesByGender(String year, String gender) {
+		return 0;
+	}
+
+	/**
+	 * Task 5. Return an approximate count of the total number of babies in a year,
+	 * regardless of gender.
+	 * 
+	 * @param year
+	 * @return the number of babies born
+	 */
+	public int numBabiesBorn(String year) {
+		return 0;
+	}
 
 }
