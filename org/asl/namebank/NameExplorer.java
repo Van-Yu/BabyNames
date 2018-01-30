eca22c4a0a82dfa9ba4216f3b740717d4a6e4a44
kentc14 <kentc14@10.30.4.148>
2018-01-30 10:40:44 +0000

    static methods instead of instantiation

--- org/asl/namebank/NameExplorer.java
+++ org/asl/namebank/NameExplorer.java
@@ -8,7 +8,7 @@ public class NameExplorer {
 	 * @param year
 	 * @return the name with the most occurrences
 	 */
-	public String mostPopularNameForYear(String year) {
+	public static String mostPopularNameForYear(String year) {
 		return null;
 	}
 
@@ -19,7 +19,7 @@ public class NameExplorer {
 	 * @param gender
 	 * @return the name with the most occurrences
 	 */
-	public String mostPopularNameForYearByGender(String year,
+	public static String mostPopularNameForYearByGender(String year,
 			String gender) {
 		return null;
 	}
@@ -31,7 +31,7 @@ public class NameExplorer {
 	 * @param year
 	 * @return an array holding, in order, the ten most popular names for that year
 	 */
-	public String[] topTenForYear(String year) {
+	public static String[] topTenForYear(String year) {
 		return null;
 	}
 
@@ -42,7 +42,7 @@ public class NameExplorer {
 	 * @param year
 	 * @return the number of babies born with the indicated gender
 	 */
-	public int numBabiesByGender(String year, String gender) {
+	public static int numBabiesByGender(String year, String gender) {
 		return 0;
 	}
 
@@ -53,7 +53,7 @@ public class NameExplorer {
 	 * @param year
 	 * @return the number of babies born
 	 */
-	public int numBabiesTotal(String year) {
+	public static int numBabiesTotal(String year) {
 		return 0;
 	}
 
@@ -67,14 +67,44 @@ public class NameExplorer {
 	 *            name of interest
 	 * @return the NameData object of the year that name was most popular
 	 */
-	public NameData getMostPopularYear(String name) {
+	public static NameData getMostPopularYear(String name) {
 		return null;
 	}
 
 	/**
 	 * Task 7. What name has been assigned to the most babies on record?
 	 */
-	public String mostFrequentlyAssignedName() {
+	public static String mostFrequentlyAssignedName() {
 		return null;
 	}
+	
+	public static void main(String[] args) {
+		// Task 1.
+		String mp1964 = mostPopularNameForYear("1964"); // Michael
+		System.out.println(
+				"The most popular name for 1964: " + mp1964);
+		String mp1987 = mostPopularNameForYear("1987"); // Michael
+		System.out.println(
+				"The most popular name for 1987: " + mp1987);
+		// Task 2.
+		String mpg2001 = mostPopularNameForYearByGender("2001", "M");
+		System.out.println(
+				"The most popular male name for 2001: " + mpg2001); // Michael
+		// Task 3.
+
+		// Task 4.
+
+		// Task 5
+
+		// Task 6
+
+		// Task 7
+
+		/** How might you go about answering the following questions: */
+		// Is Michael always the most popular name given in a year?
+		// When was the height of popularity of your own name?
+		// How many different names were given to girls in 1880? in 1980?
+		// How many names account for 50% of the babies named in 1880? in 1980?
+	}
+
 }
