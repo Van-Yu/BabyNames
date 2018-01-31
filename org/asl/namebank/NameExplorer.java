1161dc7961f8132cf87fc05c00eb934703b31d47
Kent Collins <kent.r.collins@gmail.com>
2018-01-31 05:14:27 +0000

    added two extreme challenges for the undaunted

--- org/asl/namebank/NameExplorer.java
+++ org/asl/namebank/NameExplorer.java
@@ -77,8 +77,8 @@ public class NameExplorer {
 	 */
 	public static String mostFrequentlyAssignedName() {
 		return null;
-	}	
-	
+	}
+
 	public static void main(String[] args) {
 		// Task 1.
 		String mp1964 = mostPopularNameForYear("1964"); // Michael
@@ -106,6 +106,21 @@ public class NameExplorer {
 		// How many different names were given to girls in 1880? in 1980?
 		// How many names account for 50% of the babies named in 1880? in 1980?
 		// What was the most popular initial letter for a given year?
+
+		/**
+		 * Extreme challenge 1. Identify the names in a year that had repeated letters,
+		 * such as the "n" in "Donna", the "s" in "Massimo", or the "a" in "Pumbaa".
+		 * Prepare a list/array of these names in alphabetical order of the repeated
+		 * letter, ie. {"Pumbaa", "Donna", "Massimo"}
+		 */
+
+		/**
+		 * Extreme challenge 2. Which names entered the top 10 for their gender in a
+		 * dramatic or explosive way? You might determine the percent change for each
+		 * name that was NOT in the top 10 the previous year and from those, identify
+		 * the name that entered the top 10 with the highest percentage change. Can you
+		 * link this suddenly popular name to a movie, TV show, or historic occurrence?
+		 */
 	}
 
 }
